f601a37a71e6179861d9c1162249676226adaaff
이건우 <yja20850@naver.com>
2024-10-08 12:18:54 +0900

    setting/#7/prisma 파일 구조 세팅

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Users } from '@prisma/client';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
