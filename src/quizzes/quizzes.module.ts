f1377af9ca2af4b41953d8c619f10627b0c9b262
dg1418 <dg1418>
2024-10-16 20:18:49 +0900

    refec/#8/필요없는 코드 수정

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { QuizzesService } from './quizzes.service';
 import { QuizzesController } from './quizzes.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [QuizzesController],
