9707c315fec313c55f5488ae7fe8267e1fca65bd
이건우 <yja20850@naver.com>
2024-10-17 20:50:55 +0900

    feat/#16/point update 요청에 간단히 세팅

--- src/users/controllers/user-point.controller.ts
+++ src/users/controllers/user-point.controller.ts
@@ -8,7 +8,14 @@ import {
 } from '@nestjs/common';
 import { UserPointService } from '../services/user-point.service';
 import { UpdatePointDto } from '../dtos/update-point.dto';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('point')
 @Controller('users/:id/point')
 export class UserPointController {
   constructor(private readonly pointsService: UserPointService) {}
@@ -19,6 +26,17 @@ export class UserPointController {
   }
 
   @Patch()
+  @ApiOkResponse({ description: '성공적으로 포인트가 수정됨' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiOperation({
+    summary: '포인트 증감 수정',
+    description: ` ## 포인트 증감 수정
+    0 포인트 밑으로 수정되지 않음`,
+    externalDocs: {
+      description: 'nest 공식 문서',
+      url: 'https://docs.nestjs.com',
+    },
+  })
   update(
     @Param('id', ParseIntPipe) userId: number,
     @Body() updatePointData: UpdatePointDto,
--- src/users/dtos/update-point.dto.ts
+++ src/users/dtos/update-point.dto.ts
@@ -1,10 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UpdatePointDto {
+  @ApiProperty({
+    required: false,
+    type: String,
+    description: '유저 닉네임',
+    example: 'gwgw123',
+    default: 'nickname',
+    minimum: 2,
+    maximum: 20,
+  })
   @IsOptional()
   @IsString()
   readonly nickname?: string;
 
+  @ApiProperty({
+    required: true,
+    type: Number,
+    description: '포인트 증감치',
+    example: 30,
+    default: 30,
+  })
   @IsNumber()
   readonly point: number;
 }
