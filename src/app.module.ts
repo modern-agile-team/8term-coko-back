1a1ee425ad4c806de0e29ca42a1d1b7eb03fd9f5
JeonHeena <heena0727@gmail.com>
2024-10-22 18:43:35 +0900

    merge again

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UserPointModule } from './users/modules/user-point.module';
 import { UserExperienceModule } from './users/modules/user-experience.module';
 import { PrismaModule } from './prisma/prisma.module';
-<<<<<<< HEAD
 import { ItemsModule } from './items/items.module';
-
-@Module({
-  imports: [ItemsModule],
-=======
 import { UsersModule } from './users/modules/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { QuizzesModule } from './quizzes/quizzes.module';
@@ -24,8 +20,8 @@ import { SectionsModule } from './sections/sections.module';
     UserPointModule,
     UserExperienceModule,
     PrismaModule,
+    ItemsModule,
   ],
->>>>>>> fac6412d22fd787741ed23cc2c484c0383ef7f3b
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
