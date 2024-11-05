a791ce822fdc0e897df1f2cef98c2bc9817eb873
JeonHeena <heena0727@gmail.com>
2024-11-05 19:12:40 +0900

    buyItem시 정수만을 허용하도록 @IsInt로 변경

--- src/items/dto/buy-item.dto.ts
+++ src/items/dto/buy-item.dto.ts
@@ -1,9 +1,9 @@
-import { IsNumber } from 'class-validator';
+import { IsInt, IsNumber } from 'class-validator';
 
 export class BuyItemDto {
-  @IsNumber()
+  @IsInt()
   userId: number;
 
-  @IsNumber()
+  @IsInt()
   itemId: number;
 }
