617f26b735ffa9e5b436743d493248ec8a078268
이건우 <yja20850@naver.com>
2024-11-29 08:35:27 +0900

    feat/#28/CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,10 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://cokoedu.com'],
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
