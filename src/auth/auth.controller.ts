42a78d76dade6eb7717daf56544b0d9115cace3c
이건우 <yja20850@naver.com>
2024-11-29 22:09:06 +0900

    feat/#28/소셜로그인 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Redirect, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/common/decorators/get-user.decorator';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   // Google 로그인 시작
   @Get('google')
@@ -15,7 +20,15 @@ export class AuthController {
   // Google 로그인 콜백 처리
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  googleLogin(@User() user: any) {
-    return this.authService.googleLogin(user);
+  googleLogin(@User() user: any, @Res() res: Response) {
+    const jwtToken = this.authService.googleLogin(user);
+
+    res.cookie('jwt', jwtToken, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 3600000,
+    });
+
+    res.redirect('/learn');
   }
 }
