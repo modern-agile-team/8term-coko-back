bb3f4cb15eb8e981ea20785695beaa173d538e28
이건우 <yja20850@naver.com>
2024-12-04 18:32:24 +0900

    setting/#28/migration 세팅

--- prisma/migrations/20241204092727_/migration.sql
+++ prisma/migrations/20241204092727_/migration.sql
@@ -68,6 +68,29 @@ CREATE TABLE "parts" (
 );
 
 -- CreateTable
+CREATE TABLE "items" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "cost" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_items" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "item_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "is_equipped" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "user_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "token" (
     "id" SERIAL NOT NULL,
     "user_id" INTEGER NOT NULL,
@@ -87,6 +110,15 @@ CREATE UNIQUE INDEX "users_provider_id_key" ON "users"("provider_id");
 CREATE UNIQUE INDEX "parts_name_key" ON "parts"("name");
 
 -- CreateIndex
+CREATE INDEX "user_items_user_id_idx" ON "user_items"("user_id");
+
+-- CreateIndex
+CREATE INDEX "user_items_item_id_idx" ON "user_items"("item_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_items_user_id_item_id_key" ON "user_items"("user_id", "item_id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "token_user_id_key" ON "token"("user_id");
 
 -- AddForeignKey
@@ -100,6 +132,12 @@ ALTER TABLE "progress" ADD CONSTRAINT "progress_quiz_id_fkey" FOREIGN KEY ("quiz
 
 -- AddForeignKey
 ALTER TABLE "parts" ADD CONSTRAINT "parts_section_id_fkey" FOREIGN KEY ("section_id") REFERENCES "sections"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_items" ADD CONSTRAINT "user_items_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_items" ADD CONSTRAINT "user_items_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "token" ADD CONSTRAINT "token_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
