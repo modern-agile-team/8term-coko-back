7da3e491f3ae054b2a4391b40c8db9f7c74c94ec
dg1418 <dg1418@naver.com>
2024-12-08 18:21:02 +0900

    refactor/#35/ResSectionDto가 더이상 생성 일자와 업데이트 일자를 주지 않음

--- src/sections/dto/res-section.dto.ts
+++ src/sections/dto/res-section.dto.ts
@@ -3,15 +3,11 @@ import { Part } from '@prisma/client';
 export class ResSectionDto {
   readonly id: number;
   readonly name: string;
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
   readonly part?: Part[];
 
-  constructor({ id, name, createdAt, updatedAt, part }: ResSectionDto) {
+  constructor({ id, name, part }: ResSectionDto) {
     this.id = id;
     this.name = name;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
     this.part = part;
   }
 
