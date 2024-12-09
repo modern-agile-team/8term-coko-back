8523c4b9a79446ca36e33d5cbe6a743ee51a577d
dg1418 <dg1418@naver.com>
2024-12-09 14:35:20 +0900

    refactor/#35/progress 스웨거 설정

--- src/progress/progress.swagger.ts
+++ src/progress/progress.swagger.ts
@@ -5,18 +5,37 @@ export const ApiProgress = {
   findAll: () => {
     return applyDecorators(
       ApiOperation({
-        summary: '',
+        summary: '유저의 전체 문제 풀이 현황 조회',
       }),
       ApiResponse({
-        //status: 204,
-        //description: '새 문제가 성공적으로 생성됨',
+        status: 200,
+        description: `
+        전체 문제 수,
+        유저가 한번이라도 푼 문제 수,
+        유저가 맞힌 문제 수,
+        유저가 틀린 문제 수
+        `,
+        content: {
+          JSON: {
+            example: {
+              totalQuizCount: 2,
+              totalUserProgressCount: 1,
+              correctUserProgressCount: 0,
+              inCorrectUserProgressCount: 1,
+            },
+          },
+        },
       }),
     );
   },
   createOrUpdate: () => {
     return applyDecorators(
       ApiOperation({
-        summary: '',
+        summary: '유저의 문제에 대한 진행도생성 또는 업데이트',
+      }),
+      ApiResponse({
+        status: 204,
+        description: 'progress가 성공적으로 생성되었거나 업데이트 됨',
       }),
     );
   },
