251073bbf6f14f35576cf1a692a4ab84f6d5872d
dg1418 <dg1418@naver.com>
2024-12-09 23:02:08 +0900

    feat/#44/part-progress Entity 및 enum 생성

--- src/part-progress/entities/part-progress.entity.ts
+++ src/part-progress/entities/part-progress.entity.ts
@@ -1,1 +1,24 @@
-export class PartProgress {}
+interface PartProgressModel {
+  userId: number;
+  partId: number;
+  status: PartStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const PartStatus = {
+  LOCKED: 'LOCKED',
+  STARTED: 'STARTED',
+  IN_PROGRESS: 'IN_PROGRESS',
+  COMPLETED: 'COMPLETED',
+} as const;
+
+export type PartStatus = (typeof PartStatus)[keyof typeof PartStatus];
+
+export class PartProgress implements PartProgressModel {
+  userId: number;
+  partId: number;
+  status: PartStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
