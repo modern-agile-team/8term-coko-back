c847b8c7073f2574d9e916f47630120067f14e12
JeonHeena <heena0727@gmail.com>
2024-12-11 18:21:51 +0900

    feat/#41/items.module주석삭제

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,4 +1,3 @@
-// module : service와 controller를 NestJS에 등록한다
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
