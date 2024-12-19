2a280e7342aa42c2ddcda66d21217d75b6f02a96
이건우 <yja20850@naver.com>
2024-12-19 16:50:21 +0900

    setting/#48/develop pull

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
-import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import * as jwt from 'jsonwebtoken';
 import { UsersService } from 'src/users/services/users.service';
 import { RedisService } from '../redis/redis.service';
