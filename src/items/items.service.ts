8ed1ec0f2d046cabe344d390178692d8d2dab1d6
JeonHeena <heena0727@gmail.com>
2025-01-07 12:36:15 +0900

    feat/#41/아이템 조회 기능

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -10,9 +10,26 @@ import { BuyItemDto, ItemChangeStatusDto } from './dto/change-item-status.dto';
 export class ItemsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  //전체 아이템 조회 + 필터링 getItems
-  getItems(mainCategoryId?: number, subCategoryId?: number) {
-    return this.prisma.item.findMany();
+  //전체 아이템 조회 + 필터링
+  async getItems(mainCategoryId?: number, subCategoryId?: number) {
+    //필터링 조건
+    const items = await this.prisma.item.findMany({
+      //items변수 = 아이템 전부 가져오기
+      where: {
+        ...(mainCategoryId && { mainCategoryId: mainCategoryId as number }), //mainCategoryId가 truthy -> {mainCategoryId: mainCategoryId} 객체 반환 , falsy -> undefined
+        ...(subCategoryId && { subCategoryId: subCategoryId as number }), //subCategoryId가 truthy -> {subCategoryId: subCategoryId} 객체 반환, falsy -> undefined
+      },
+      include: {
+        mainCategory: true, //mainCategory 정보도 가져온다
+        subCategory: true, //subCategory 정보도 가져온다
+      },
+    });
+
+    //필터링 된 아이템이 없을 경우
+    if (items.length === 0) {
+      throw new NotFoundException('Items not found');
+    }
+    return items;
   }
 
   //아이템 추가 addItem (개발/테스트 용)
