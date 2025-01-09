0902afb369bdcc74c0a598beb1362210487b9337
JeonHeena <heena0727@gmail.com>
2025-01-09 14:20:59 +0900

    docs/#41/items.controller 필요없는 api 삭제

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,23 +1,15 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Param,
-  Body,
-  HttpCode,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, HttpCode, Query } from '@nestjs/common';
 import { ItemsService } from './items.service';
-import { ItemChangeStatusDto, BuyItemDto } from './dto/change-item-status.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiItems } from './items.swagger';
 
 @ApiTags('items')
-@Controller('items') // '/items'경로 요청 처리
+@Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   // GET /items : 전체 아이템 조회 + 필터링 추가(maincategory, subcategory : 카테고리의 아이템 조회)
+  //**프론트-> 어떻게 보내는 게 맞을지 대화 필요 *//
   @Get()
   @ApiItems.getItems()
   async getItems(
@@ -25,28 +17,5 @@ export class ItemsController {
     @Query('subcategory') subCategoryId?: number,
   ) {
     return await this.itemsService.getItems(mainCategoryId, subCategoryId);
-  }
-
-  // Post /items/dev 요청 처리 (개발환경에서만 실행되도록 한다)
-  @Post()
-  @HttpCode(201)
-  @ApiItems.addItem()
-  async addItem(@Body() addItemDto: ItemChangeStatusDto): Promise<void> {
-    await this.itemsService.addItem(addItemDto);
-  }
-
-  // POST /users/:userId/items/:itemId 요청 처리
-  @Post('users/:userId/items/:itemId')
-  @HttpCode(204)
-  @ApiItems.buyItem()
-  async buyItem(@Body() buyItemDto: BuyItemDto): Promise<void> {
-    await this.itemsService.buyItem(buyItemDto);
-  }
-
-  // GET /users/:userId/items 요청 처리
-  @Get('users/:userId')
-  @ApiItems.getUserItems()
-  async getUserItems(@Param('userId') userId: number) {
-    return await this.itemsService.getUserItems(userId);
   }
 }
