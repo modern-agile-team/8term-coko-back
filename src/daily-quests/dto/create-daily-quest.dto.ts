fbf9e9d968abc5ee6776e38c056908f7381391b9
dg1418 <dg1418@naver.com>
2025-01-22 17:02:52 +0900

    feat/#73/create DTO 작성

--- src/daily-quests/dto/create-daily-quest.dto.ts
+++ src/daily-quests/dto/create-daily-quest.dto.ts
@@ -1,1 +1,21 @@
-export class CreateDailyQuestDto {}
+import { IsInt, IsString, Min } from 'class-validator';
+
+export class CreateDailyQuestDto {
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly content: string;
+
+  @IsInt()
+  @Min(0)
+  readonly point: number;
+
+  @IsInt()
+  @Min(0)
+  readonly exp: number;
+
+  @IsInt()
+  @Min(0)
+  readonly condition: number;
+}
