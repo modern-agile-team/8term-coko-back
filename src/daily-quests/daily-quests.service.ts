b9484812171f8c86cd16dbb0d491e4b1d10681c0
dg1418 <dg1418@naver.com>
2025-01-22 16:49:06 +0900

    feat/#73/서비스 간단 작성

--- src/daily-quests/daily-quests.service.ts
+++ src/daily-quests/daily-quests.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDailyQuestDto } from './dto/create-daily-quest.dto';
 import { UpdateDailyQuestDto } from './dto/update-daily-quest.dto';
 
 @Injectable()
 export class DailyQuestsService {
-  create(createDailyQuestDto: CreateDailyQuestDto) {
-    return 'This action adds a new dailyQuest';
+  constructor(private readonly dailyQuestsRepository: any) {}
+
+  async findAll() {
+    return this.dailyQuestsRepository.findAll();
   }
 
-  findAll() {
-    return `This action returns all dailyQuests`;
+  async findOne(questId: number) {
+    const dailyQuest = await this.dailyQuestsRepository.findOne(questId);
+
+    if (!dailyQuest) {
+      throw new NotFoundException(`ID ${questId} 를 찾을 수 없습니다.`);
+    }
+
+    return dailyQuest;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dailyQuest`;
+  async create(body: CreateDailyQuestDto) {
+    return this.dailyQuestsRepository.create(body);
   }
 
-  update(id: number, updateDailyQuestDto: UpdateDailyQuestDto) {
-    return `This action updates a #${id} dailyQuest`;
+  async update(questId: number, body: UpdateDailyQuestDto) {
+    await this.findOne(questId);
+    return this.dailyQuestsRepository.update(questId, body);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} dailyQuest`;
+  async remove(questId: number) {
+    await this.findOne(questId);
+    return this.dailyQuestsRepository.remove(questId);
   }
 }
