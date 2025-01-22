581623daed69804c908db35709c5d43cf9b72c89
dg1418 <dg1418@naver.com>
2025-01-22 18:03:11 +0900

    feat/#73/respose daily quest DTO 생성

---
+++ src/daily-quests/dto/res-daily-quest.dto.ts
@@ -0,0 +1,19 @@
+import { DailyQuest } from '../entities/daily-quest.entity';
+
+export class ResDailyQuestDto {
+  readonly id: number;
+  readonly title: string;
+  readonly content: string;
+  readonly point: number;
+  readonly exp: number;
+  readonly condition: number;
+
+  constructor(dailyQuest: DailyQuest) {
+    this.id = dailyQuest.id;
+    this.title = dailyQuest.title;
+    this.content = dailyQuest.content;
+    this.point = dailyQuest.point;
+    this.exp = dailyQuest.exp;
+    this.condition = dailyQuest.condition;
+  }
+}
