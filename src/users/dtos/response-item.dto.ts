409d7aeb8716127f0e4fddaa3f72e50b26f964b8
JeonHeena <heena0727@gmail.com>
2025-01-23 20:01:25 +0900

    docs/#41/getUserItems dto Swagger 파일 작성

--- src/users/dtos/response-item.dto.ts
+++ src/users/dtos/response-item.dto.ts
@@ -1,11 +1,53 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ResponseItemDto {
-  readonly id: number; //UserItem id
+  @ApiProperty({
+    description: '아이템 ID',
+    example: 1,
+  })
+  readonly id: number;
+
+  @ApiProperty({
+    description: '아이템 이름',
+    example: '검정 모자',
+  }) //UserItem id
   readonly name: string;
+
+  @ApiProperty({
+    description: '아이템 가격',
+    example: 2000,
+  })
   readonly price: number;
+
+  @ApiProperty({
+    description: '아이템 이미지 URL',
+    example: 'https://example.com/blackhatImage.jpg',
+  })
   readonly image: string;
+
+  @ApiProperty({
+    description: '메인 카테고리 ID',
+    example: 2,
+  })
   readonly mainCategoryId: number;
+
+  @ApiProperty({
+    description: '서브 카테고리 ID',
+    example: 1,
+    nullable: true,
+  })
   readonly subCategoryId: number | null;
+
+  @ApiProperty({
+    description: '아이템 착용 여부',
+    example: false,
+  })
   readonly isEquipped: boolean;
+
+  @ApiProperty({
+    description: '아이템 구매 일시',
+    example: '2025-01-23T12:00:00Z',
+  })
   readonly purchasedAt: Date;
 
   constructor(item: any, userItem?: any) {
