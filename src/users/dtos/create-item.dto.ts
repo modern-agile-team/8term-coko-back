55cb5459df734a37f517d3e340a3b9675f1e1367
JeonHeena <heena0727@gmail.com>
2025-01-24 11:48:47 +0900

    docs/#41/create-item.dto 파일 수정(구매할 아이템 최소(1),최대값(10) 설정)

--- src/users/dtos/create-item.dto.ts
+++ src/users/dtos/create-item.dto.ts
@@ -1,9 +1,11 @@
-import { IsArray, IsInt } from 'class-validator';
+import { IsArray, IsInt, ArrayMinSize, ArrayMaxSize } from 'class-validator';
 export class CreateItemDto {
   @IsInt()
   userId: number;
 
   @IsArray()
   @IsInt({ each: true })
+  @ArrayMinSize(1)
+  @ArrayMaxSize(10)
   itemIds: number[];
 }
