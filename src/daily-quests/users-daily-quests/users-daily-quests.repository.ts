b4088251de6bc446b291cf711f671cc68639d48e
dg1418 <dg1418@naver.com>
2025-02-05 16:02:52 +0900

    feat/#103/유저일일퀘스트 리포지토리 작성

---
+++ src/daily-quests/users-daily-quests/users-daily-quests.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUsersDailyQuestDto } from './dto/update-users-daily-quest.dto';
+
+@Injectable()
+export class UsersDailyQuestsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAllByUserId(userId: number) {
+    return this.prisma.userDailyQuest.findMany({
+      where: { userId },
+    });
+  }
+
+  async findOneById(id: number) {
+    return this.prisma.userDailyQuest.findUnique({
+      where: { id },
+    });
+  }
+
+  async create(userId: number, dailyQuestId: number) {
+    return this.prisma.userDailyQuest.create({
+      data: { userId, dailyQuestId },
+    });
+  }
+
+  async updateById(id: number, data: UpdateUsersDailyQuestDto) {
+    return this.prisma.userDailyQuest.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteAll() {
+    return this.prisma.userDailyQuest.deleteMany();
+  }
+}
