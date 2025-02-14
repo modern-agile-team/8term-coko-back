0699a9a7818622cdee483c3d1570456a2f0331a6
dg1418 <dg1418@naver.com>
2025-02-14 11:40:44 +0900

    feat/#123/컨트롤러 작성

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,34 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  HttpCode,
+} from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
 import { CreateChallengeDto } from './dto/create-challenge.dto';
 import { UpdateChallengeDto } from './dto/update-challenge.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { PositiveIntPipe } from 'src/common/pipes/positive-int/positive-int.pipe';
 
 @Controller('challenge')
 export class ChallengeController {
   constructor(private readonly challengeService: ChallengeService) {}
 
-  @Post()
-  create(@Body() createChallengeDto: CreateChallengeDto) {
-    return this.challengeService.create(createChallengeDto);
-  }
-
   @Get()
-  findAll() {
-    return this.challengeService.findAll();
+  //@ApiChallenge.findAll()
+  async findAll() {
+    await this.challengeService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.challengeService.findOne(+id);
+  @Get(':challengeId')
+  //@ApiChallenge.findOne()
+  async findOne(@Param('challengeId', PositiveIntPipe) challengeId: number) {
+    await this.challengeService.findOne(challengeId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChallengeDto: UpdateChallengeDto) {
-    return this.challengeService.update(+id, updateChallengeDto);
+  @Post()
+  //@ApiChallenge.create()
+  @HttpCode(204)
+  //@UseGuards(AuthGuard('adminAccessToken'))
+  async create(@Body() body: CreateChallengeDto) {
+    await this.challengeService.create(body);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.challengeService.remove(+id);
+  @Patch(':challengeId')
+  //@ApiChallenge.update()
+  @HttpCode(204)
+  //@UseGuards(AuthGuard('adminAccessToken'))
+  async update(
+    @Param('challengeId', PositiveIntPipe) challengeId: number,
+    @Body() body: UpdateChallengeDto,
+  ) {
+    await this.challengeService.update(challengeId, body);
+  }
+
+  @Delete(':challengeId')
+  //@ApiChallenge.remove()
+  @HttpCode(204)
+  //@UseGuards(AuthGuard('adminAccessToken'))
+  async remove(@Param('challengeId', PositiveIntPipe) challengeId: number) {
+    await this.challengeService.remove(challengeId);
   }
 }
--- src/challenge/challenge.service.ts
+++ src/challenge/challenge.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChallengeDto } from './dto/create-challenge.dto';
 import { UpdateChallengeDto } from './dto/update-challenge.dto';
+import { ChallengeRepository } from './challenge.repository';
 
 @Injectable()
 export class ChallengeService {
-  create(createChallengeDto: CreateChallengeDto) {
-    return 'This action adds a new challenge';
-  }
+  constructor(private readonly challengeRepository: ChallengeRepository) {}
 
   findAll() {
-    return `This action returns all challenge`;
+    return `오프셋 페이지네이션`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} challenge`;
   }
 
-  update(id: number, updateChallengeDto: UpdateChallengeDto) {
+  create(body: CreateChallengeDto) {
+    return 'This action adds a new challenge';
+  }
+
+  update(id: number, body: UpdateChallengeDto) {
     return `This action updates a #${id} challenge`;
   }
 
