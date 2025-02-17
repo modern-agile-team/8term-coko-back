379b8911d2d878a0ede5a5702d41333ce34a6560
이건우 <yja20850@naver.com>
2025-02-18 00:00:05 +0900

    fix/#126/hp refill 시간 수정

--- src/users/constants/user-hp.constant.ts
+++ src/users/constants/user-hp.constant.ts
@@ -5,5 +5,6 @@ export const HP_DECREASE_VALUE: number = 1;
 
 /**
  * hp refill 시간
+ * 현재는 20분으로 설정
  */
-export const HP_REFILL_TIME: number = 10 * 1000;
+export const HP_REFILL_TIME: number = 20 * 60 * 1000;
