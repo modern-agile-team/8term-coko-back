50691262c9fab6dfc4725517139d687fad4cb98d
JeonHeena <heena0727@gmail.com>
2025-03-04 17:18:08 +0900

    feat/#148/merge에러 해결

--- src/users/services/user-items.service.ts
+++ src/users/services/user-items.service.ts
@@ -7,17 +7,15 @@ import {
 import { PrismaService } from 'src/prisma/prisma.service';
 import { BuyUserItemsDto } from '../dtos/buy-userItems.dto';
 import { EquipUseritemDto } from '../dtos/equip-useritem.dto';
-
 import { UserItemsPaginationQueryDto } from '../dtos/userItems-pagination-query.dto';
 import { UserItemsRepository } from '../repositories/user-items.repository';
 import { UserItemsPaginationResponseDto } from '../dtos/response-userItems-pagination.dto';
 import { UserItem } from '../entities/user-item.entity';
 import { Item } from 'src/items/entities/item.entity';
 import { UserItemsQueryDto } from '../dtos/userItems-query.dto';
-import { ResponseItemDto } from '../dtos/response-item.dto';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { EVENT } from 'src/challenges/const/challenges.constant';
-
+import { ResponseUserItemDto } from '../dtos/response-useritem.dto';
 
 @Injectable()
 export class UserItemsService {
@@ -70,8 +68,8 @@ export class UserItemsService {
         }
         return { ...userItem, item };
       }),
-
-
+    );
+  }
   //사용자 아이템 목록 조회
   async getUserItems(userId: number) {
     const userItems = await this.prisma.userItem.findMany({
@@ -89,7 +87,7 @@ export class UserItemsService {
       throw new NotFoundException('아이템을 찾을 수 없습니다.');
     }
     return userItems.map(
-      (userItem) => new ResponseItemDto(userItem.item, userItem),
+      (userItem) => new ResponseUserItemDto(userItem.item, userItem),
     );
 
     const totalCount = contents.length;
