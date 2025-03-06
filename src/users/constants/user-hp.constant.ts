9e1bd5952adee0c2b6089875c26da715f9f8c457
이건우 <yja20850@naver.com>
2025-03-06 21:02:45 +0900

    fix/#188/이벤트 상수 시간 변경

--- src/users/constants/user-hp.constant.ts
+++ src/users/constants/user-hp.constant.ts
@@ -5,7 +5,6 @@ export const HP_DECREASE_VALUE: number = 1;
 
 /**
  * hp refill 시간
- * 10분으로 설정 (25.02.18)
- * 테스트 환경 10초로 설정
+ * 3분으로 설정 (25.03.06)
  */
-export const HP_REFILL_TIME: number = 10 * 1000;
+export const HP_REFILL_TIME: number = 3 * 60 * 1000;
