8af4691297e818500f3c6c845aa4a0c0f48040dc
이건우 <yja20850@naver.com>
2025-03-14 15:11:11 +0900

    fix/#200/로그 필터 수정

--- src/logger/logger.filters.ts
+++ src/logger/logger.filters.ts
@@ -1,14 +1,14 @@
 import * as winston from 'winston';
 
 /**
- * error, critical 로그만 통과시키는 필터
+ * error 로그만 통과시키는 필터
  */
 export const errorOnlyFilter = winston.format((info) => {
-  return info.level === 'error' || info.level === 'critical' ? info : false;
+  return info.level === 'error' ? info : false;
 });
 
 /**
- * error, critical이 아닌 로그만 통과시키는 필터
+ * error이 아닌 로그만 통과시키는 필터
  */
 export const nonErrorFilter = winston.format((info) => {
   return info.level !== 'error' ? info : false;
