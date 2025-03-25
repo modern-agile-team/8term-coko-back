239ff9b2fab470ead281fbaab28a47f3c50edeec
이건우 <yja20850@naver.com>
2025-03-25 14:59:22 +0900

    fix/#217/s3 버전 수정

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,33 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as AWS from 'aws-sdk';
+import { S3Client } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
 import * as fs from 'fs';
 
 @Injectable()
 export class S3Service {
-  private s3: AWS.S3;
+  private s3Client: S3Client;
   private bucketName: string;
 
   constructor(private readonly configService: ConfigService) {
     this.bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
-    this.s3 = new AWS.S3({
-      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+    this.s3Client = new S3Client({
       region: this.configService.get<string>('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get<string>(
+          'AWS_SECRET_ACCESS_KEY',
+        ),
+      },
     });
   }
 
-  async uploadFile(
-    filePath: string,
-    s3Key: string,
-  ): Promise<AWS.S3.ManagedUpload.SendData> {
+  async uploadFile(filePath: string, s3Key: string): Promise<any> {
     const fileStream = fs.createReadStream(filePath);
-    const params = {
+    const uploadParams = {
       Bucket: this.bucketName,
       Key: s3Key,
       Body: fileStream,
     };
 
-    return await this.s3.upload(params).promise();
+    // Upload 클래스를 사용하여 멀티파트 업로드 지원
+    const parallelUpload = new Upload({
+      client: this.s3Client,
+      params: uploadParams,
+    });
+
+    return await parallelUpload.done();
   }
 }
