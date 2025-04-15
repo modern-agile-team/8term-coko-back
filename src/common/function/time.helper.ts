fb87f4f6a2f9d3fc55d9ca6a72acc76dc45262fa
이건우 <yja20850@naver.com>
2025-04-16 00:59:59 +0900

    feat/#220/어제 날짜 반환하는 함수 생성

--- src/common/function/time.helper.ts
+++ src/common/function/time.helper.ts
@@ -6,3 +6,13 @@ export function nowKST(): Date {
   const now = new Date();
   return new Date(now.getTime() + 9 * 60 * 60 * 1000); // UTC → KST 변환
 }
+
+/**
+ * 전날의 KST 시간 반환
+ * @returns
+ */
+export function yesterdayKST(): Date {
+  const now = new Date();
+  const nowKST = new Date(now.getTime() + 9 * 60 * 60 * 1000); // UTC → KST 변환
+  return new Date(nowKST.getTime() - 24 * 60 * 60 * 1000);
+}
